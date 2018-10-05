f00b8ac3270741612a0c0fb7709c9d717284afc4
BrenoTeodor-o <teodoro.breno@outlook.com.br>
2018-10-04 21:20:17 -0300

    Build a simple UI

--- app/src/main/java/com/example/teodo/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/teodo/myfirstapp/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+// https://developer.android.com/training/basics/firstapp/starting-activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
