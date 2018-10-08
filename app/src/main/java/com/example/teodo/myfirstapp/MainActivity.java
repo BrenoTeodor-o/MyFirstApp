09773b762f7c8925404a615655ba98293900591e
BrenoTeodor-o <teodoro.breno@outlook.com.br>
2018-10-08 20:07:01 -0300

    Add Intents to send message to another activity

--- app/src/main/java/com/example/teodo/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/teodo/myfirstapp/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.teodo.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 // https://developer.android.com/training/basics/firstapp/starting-activity
+    public static final String EXTRA_MESSAGE = "com.teodo.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the user taps the Send button */
+    public void  sendMessage(View view){
+        //code
+        Intent intent = new Intent(this ,DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
 }
